59abc336eb71f5815b4fe3d451e4e041fce62046
Mag091986 <k9921star@gmail.com>
2024-01-19 22:25:41 -0500

    Cambios y ahora sí funciona con firebase

--- script.js
+++ script.js
@@ -8,49 +8,58 @@ const firebaseConfig = {
   measurementId: "G-H6YG0MD2VN"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+
+// Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore();
 
-document.getElementById('formulario').addEventListener('submit', (event) => {
-    event.reventDefault(); // Corrección: es preventDefault(), no preventDefaul()
 
-    let entradaNombre = document.getElementById('name');
-    let errorNombre = document.getElementById('nameError');
+document.getElementById('formulario').addEventListener('submit', (event) =>{
+    event.preventDefault()
 
-    if (entradaNombre.value.trim() === '') {
-        errorNombre.textContent = 'Por favor introduce tu nombre';
-        errorNombre.classList.add('error-message');
-    } else {
-        errorNombre.textContent = '';
-        errorNombre.classList.remove('error-message');
+    //Validar campo nombre
+    let entradaNombre = document.getElementById('name')
+    let errorNombre = document.getElementById('nameError')
+
+    if(entradaNombre.value.trim() === ''){
+        errorNombre.textContent = 'Por favor, introducí tu nombre'
+        errorNombre.classList.add('error-message')
+    }else{
+        errorNombre.textContent = ''
+        errorNombre.classList.remove('error-message')
     }
 
-    let emailEntrada = document.getElementById('email');
-    let emailError = document.getElementById('emailError');
-    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailPattern.test(emailEntrada.value)){
-        emailError.textContent = 'Por favor introduce un correo válido';
-        emailError.classList.add('error-message');
-    } else {
-        emailError.textContent = '';
-        emailError.classList.remove('error-message');
+    //Validar correo electrónico
+    let emailEntrada = document.getElementById('email')
+    let emailError = document.getElementById('emailError')
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Patrón de validación básico
+    if(!emailPattern.test(emailEntrada.value)){
+        emailError.textContent = 'Por favor, introducí un mail válido'
+        emailError.classList.add('error-message')
+    }else{
+        emailError.textContent = ''
+        emailError.classList.remove('error-message')
     }
 
-    let contrasenaEntrada = document.getElementById('password');
-    let contrasenaError = document.getElementById('passwordError');
-    let contrasenaPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
-
-    if (!contrasenaPattern.test(contrasenaEntrada.value)) {
-        contrasenaError.textContent = 'La contraseña debe contener al menos 8 caracteres, números, mayúsculas y caracteres especiales';
-        contrasenaError.classList.add('error-message');
-    } else {
-        contrasenaError.textContent = '';
-        contrasenaError.classList.remove('error-message');
+    //Validar la contraseña
+    let contrasenaEntrada = document.getElementById('password')
+    let contrasenaError = document.getElementById('passwordError')
+    let contrasenaPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/;
+    if(!contrasenaPattern.test(contrasenaEntrada.value)){
+        contrasenaError.textContent = 'La contraseña debe tener al menos 8 caracteres, números, mayúsculas y minúsculas y caracteres especiales'
+        contrasenaError.classList.add('error-message')
+    }else{
+        contrasenaError.textContent = ''
+        contrasenaError.classList.remove('error-message')
     }
 
-    if (!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent) {
+    //Si todos los campos son válidos enviar formulario
+
+    if(!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent){
+
+        //BACKEND QUE RECIBA LA INFORMACIÓN
 
         db.collection("users").add({
             nombre: entradaNombre.value,
@@ -58,14 +67,14 @@ document.getElementById('formulario').addEventListener('submit', (event) => {
             password: contrasenaEntrada.value
         })
         .then((docRef) => {
-            alert('El formulario se ha enviado con éxito', docRef.id);
+            alert('El formulario se ha enviado con éxito', docRef.id)
             document.getElementById('formulario').reset();
         })
-        .cath((error) => {
-            alert(error);
-        })
+        .catch((error) => {
+            alert(error)
+        });
 
         
-        
     }
-});
+
+})
